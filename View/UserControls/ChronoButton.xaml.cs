3bc9083afd88bcd106ec8b57b0a697b31dee6e13
Sasha <gulixandr@gmail.com>
2023-09-29 11:50:49 +0300

    rearrangement again.

---
+++ View/UserControls/ChronoButton.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System.Drawing;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace ChronoSaver.View.UserControls
+{
+
+    public partial class ChronoButton : UserControl
+    {
+        public ChronoButton()
+        {
+            InitializeComponent();
+        }
+
+        private string _contentText = string.Empty;
+
+        public string ContentText
+        {
+            get { return _contentText; }
+            set
+            {
+                _contentText = value;
+                btn.Content = _contentText;
+            }
+        }
+
+        private Brush _color;
+        public Brush Color
+        {
+            get { return _color; }
+            set
+            {
+                _color = value;
+                btn.Background = _color;
+            }
+        }
+    }
+}
